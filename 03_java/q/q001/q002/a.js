571ee8c7479d85f178154a2ae789c1de3f61d8f6
hanjay3757 <hanjay3757@gmail.com>
2024-08-05 12:15:41 +0900

    feat: 백업 데이터 업로드 푸시

---
+++ 03_java/q/q001/q002/a.js
@@ -0,0 +1,9 @@
+// /= 대입 연산자 오른쪽에 있는걸/
+//1. cat=box >> 변수 생성 선언  var 는 varation // 
+var cat;
+//2. 박스에 물체 넣기 문자열//
+cat = "<img src='./cat.jpg'>";
+
+//3.출력//
+
+document.write(cat);
---
+++ 03_java/q/q01/q001.js
@@ -0,0 +1,32 @@
+/* 표현 1 */
+// var random = Math.floor(Math.random() * 6) + 1; // 1 ~ n 까지 범위내에서 랜덤하게 숫자 하나 뽑아주는 애.
+// var s = "주사위를 굴려 ( " + random + " ) 이 나왔습니다.";
+// document.write(s);
+
+
+/* 표현 2 */
+// var random;
+// random = Math.floor(Math.random() * 6) + 1; // 1 ~ n 까지 범위내에서 랜덤하게 숫자 하나 뽑아주는 애.
+// document.write("주사위를 굴려 ( " + random + " ) 이 나왔습니다.");
+
+
+/* 표현 3 */
+// var random = Math.floor(Math.random() * 6) + 1; // 1 ~ n 까지 범위내에서 랜덤하게 숫자 하나 뽑아주는 애.
+// document.write("주사위를 굴려 ( " + random + " ) 이 나왔습니다.");
+
+/* 표현 4 */
+// document.write("주사위를 굴려 ( " + (Math.floor(Math.random() * 6) + 1) + " ) 이 나왔습니다.");
+
+
+
+/* 6면체 */
+var random;
+random = Math.floor(Math.random() * 99)+1; // 1 ~ n 까지 범위내에서 랜덤하게 숫자 하나 뽑아주는 애.
+var s = "주사위를 굴려 ( " + random + " ) 이 나왔습니다.";
+document.write(s);
+
+// /* 4면체 */
+// var random2;
+// random2 = Math.floor(Math.random() * 4) + 1; // 1 ~ n 까지 범위내에서 랜덤하게 숫자 하나 뽑아주는 애.
+// var s2 = "주사위를 굴려 ( " + random2 + " ) 이 나왔습니다.";
+// document.write(s2);
