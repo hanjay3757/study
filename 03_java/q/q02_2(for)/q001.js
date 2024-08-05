8457ee5c7125ac4528deb7de72b3351d0511e0a1
hanjay3757 <hanjay3757@gmail.com>
2024-08-05 12:25:42 +0900

    feat: 조건문 반복문 추가

--- 03_java/q/q02_2(for)/q001.js
+++ 03_java/q/q02_2(for)/q001.js
@@ -3,5 +3,5 @@ for(var i=1;i<5;i=i+1){
         document.write("<img src='../q01/cat.jpg'</img>");
 }document.write("aaa");
 
-
+// j++ = j +1 을 줄임 표현
 
--- js/j01/a.js
+++
@@ -1,2 +0,0 @@
-document.write("<h1>g</h1>");
-document.write("<img src='cat.jpg'>핼로우월드</img>");
