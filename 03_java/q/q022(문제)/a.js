18ac5175210a4d21339104db78b6e3558bd2ea2a
hanjay3757 <hanjay3757@gmail.com>
2024-08-06 15:05:53 +0900

    function: 함수화

--- 03_java/q/q022(문제)/a.js
+++ 03_java/q/q022(문제)/a.js
@@ -1,17 +1,21 @@
-function bb(){const age = ["1살", "2살", "3살", "4살", "5살", "6살"];
+function bb(n){
+for(i=1; i<=n; i++){
+       var cata
+    var catb
+    var catc
+const age = ["1살", "2살", "3살", "4살", "5살", "6살"];
 const sp = ["페르시안", "렉돌", "코숏", "러시안블루", "폴드"];
 const animal = ["야옹", "새싹", "초롱", "별", "화", "토"];
 const ageIndex = Math.floor(Math.random()*age.length);
 const spIndex = Math.floor(Math.random()*sp.length);
 const animalIndex = Math.floor(Math.random() * animal.length);
-var catb
-var catc
+
 
 cata = age[ageIndex];
 catb = sp[spIndex];
 catc = animal[animalIndex];
-var v = cata+ " 먹은 " +catb+ " " + catc 
-+"이";
-alert(v);}
-bb();
-document.write(112);
+
+alert(cata+ " 먹은 " +catb+ " " + catc +"이")
+}
+}
+bb(2);
--- 03_java/q/q023(문제)/a.js
+++
@@ -1,7 +0,0 @@
-var a = Math.floor(Math.random()*10)+1;
-for(var i =1;i<=10;i++){
-    if(i%2/2){
-        continue;
-    }
-    document.write(i);
-}
