564462087d848f0f98e47e8572524dc0d689a566
hanjay3757 <hanjay3757@gmail.com>
2024-08-06 16:33:12 +0900

    test

---
+++ 03_java/t/function.js
@@ -0,0 +1,19 @@
+function hr(){
+    document.write("<hr>");
+}
+function br(){
+    document.write("<br>");
+}
+
+function r(max){
+    var x = Math.floor(Math.random()*max)+1
+    // document.write(x);
+    return x;
+    // return 
+}
+var swordRandom;
+swordRandom = r(5);
+document.write(swordRandom);
+hr();
+var longSwordRandom = r(1000);
+document.write(longSwordRandom);
