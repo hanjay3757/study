5753145775c99eb605bad56ebb2eb6739b6e6a13
JWH <44960019+hanjay3757@users.noreply.github.com>
2024-08-26 12:07:37 +0900

    Add files via upload

---
+++ JAVA's/tutorial/etc/board_etc/src/trrr/Main.java
@@ -0,0 +1,9 @@
+package trrr;
+
+public class Main {
+	public static void main(String[] args) {
+		Board board = new Board();
+
+		board.run();
+	}
+}
---
+++ JAVA's/tutorial/etc/board_etc/src/trrr/Post.java
@@ -0,0 +1,29 @@
+package trrr;
+
+public class Post {
+	int no = 0;
+	String title;
+	String content;
+	String writer;
+
+	public Post(int no, String title, String content, String writer) {
+		this.no = no;
+		this.title = title;
+		this.content = content;
+		this.writer = writer;
+	}
+
+	void info() {
+		String s = String.format("글번호: %s 제목: %s 작성자: %s", no, title, writer);
+		System.out.println(s);
+	}
+
+	void infoForRead() {
+		String s = String.format("글번호: %s 제목: %s 작성자: %s", no, title, writer);
+		System.out.println("================================================");
+		System.out.println(s);
+		System.out.println("================================================");
+		System.out.println(content);
+		System.out.println("================================================");
+	}
+}
