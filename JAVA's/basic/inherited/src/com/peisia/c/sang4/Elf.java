f0d4ab3563fc78ef86dc9aeed2c487e3c60ff455
JWH <44960019+hanjay3757@users.noreply.github.com>
2024-08-27 17:28:20 +0900

    Add files via upload

--- JAVA's/basic/inherited/src/com/peisia/c/sang4/Elf.java
+++ JAVA's/basic/inherited/src/com/peisia/c/sang4/Elf.java
@@ -2,17 +2,17 @@ package com.peisia.c.sang4;
 
 import com.peisia.c.util.So;
 
-public class Elf extends Monster{
-
+public class Elf extends Monster {
+//extand 상속할 주소 복사 해서 적용시키는 명령어
 	@Override
 	void attack(Player p) {
 		attack = 7;
-		So.ln(String.format("활로 %s에게 피해를 %d 줌",p.name,attack));
+		So.ln(String.format("활로 %s에게 피해를 %d 줌", p.name, attack));
 		plusPoisonDamage(p);
 	}
-	
+
 	void plusPoisonDamage(Player p) {
-		So.ln(String.format("%s에게 독데미지 %d 를 추가로 줌",p.name,attack));
+		So.ln(String.format("%s에게 독데미지 %d 를 추가로 줌", p.name, attack));
 	}
-	
+
 }
