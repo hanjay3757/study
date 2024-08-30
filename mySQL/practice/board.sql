7d9cf50826a6a626e93cd8e5aae2c4216e935662
JWH <44960019+hanjay3757@users.noreply.github.com>
2024-08-30 17:38:35 +0900

    Add files via upload

--- mySQL/practice/board.sql
+++ mySQL/practice/board.sql
@@ -1,11 +1,11 @@
 show databases;
 use my_cat;
-create table board (
-b_no int primary key auto_increment,	#글번호
+create table board(
+b_no int primary key auto_increment,
 b_title char(100),
 b_id char(20) not null,
 b_datetime datetime not null,
-b_hit int, 
+b_hit int,
 b_text text,
 b_reply_count int,
 b_reply_ori int,
@@ -13,8 +13,7 @@ b_reply_text text
 );
 
 drop table board;
-
-insert into board(b_title,b_id,b_datetime,b_text,b_hit)
+insert into board(b_title, b_id,b_datetime,b_text,b_hit)
 values(
 '헬로'
 ,
@@ -22,7 +21,7 @@ values(
 ,
 now()
 ,
-'글입니다. 글'
+'글'
 ,
 0
 );
@@ -35,11 +34,11 @@ now()
 ,
 10
 ,
-'댓댓댓'
+'adada'
 );
-
 delete from board;
 select*from board;
 select*from board where b_no=10;
-select*from board where not b_title is null;
-select*from board where b_reply_ori=10;
+select*from board where not b_no;
+select*from board where b_reply_ori = 10;
+
