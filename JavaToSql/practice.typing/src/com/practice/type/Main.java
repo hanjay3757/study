3cefc441c8b0f335a345ac2cab953d0cba73d61a
JWH <44960019+hanjay3757@users.noreply.github.com>
2024-09-05 11:50:47 +0900

    Add files via upload

---
+++ JavaToSql/practice.typing/src/com/practice/type/Main.java
@@ -0,0 +1,8 @@
+package com.practice.type;
+
+public class Main {
+	public static void main(String[] args) {
+		ProcBoard procBoard = new ProcBoard();
+		procBoard.run();
+	}
+}
---
+++ JavaToSql/practice.typing/src/com/practice/type/ProcBoard.java
@@ -0,0 +1,5 @@
+package com.practice.type;
+
+public class ProcBoard {
+
+}
