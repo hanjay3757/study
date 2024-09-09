567a55507c14420943309d4550b20e008c82ebcd
JWH <44960019+hanjay3757@users.noreply.github.com>
2024-09-09 16:27:33 +0900

    Add files via upload

--- PrivatePJ/board/Tier/src/com/tierconnect/ProcBoard.java
+++ PrivatePJ/board/Tier/src/com/tierconnect/ProcBoard.java
@@ -7,30 +7,29 @@ public class ProcBoard {
 			Db.dbPostCount();
 			Ci.r("[l]로그아웃 [b]a등급 [t]b등급 [e]프로그램종료 ");
 			Db.getPostCount();
-				String cmd = Ci.r("명령입력");
-				switch (cmd) {
-				case "1": // 글리스트
-					ProcList.run();
-					break;
-				case "2": // 글읽기
-					ProcRead.run();
-					break;
-				case "3": // 글쓰기
-					ProcWrite.run();
-					break;
-				case "4": // 글삭제
-					ProcDel.run();
-					break;
-				case "5": // 글수정
-					ProcEdit.run();
-					break;
-				case "6": // 글리스트-검색
-					ProcList.search();
-					break;
-				case "x": // 게시판 종료
-					Cw.wn("사이트 메인으로 이동");
-					break loop;
-				}
+			String cmd = Ci.r("명령입력");
+			switch (cmd) {
+			case "1": // 글리스트
+				ProcList.run();
+				break;
+			case "2": // 글읽기
+				ProcRead.run();
+				break;
+			case "3": // 글쓰기
+				ProcWrite.run();
+				break;
+			case "4": // 글삭제
+				ProcDel.run();
+				break;
+			case "5": // 글수정
+				ProcEdit.run();
+				break;
+			case "6": // 글리스트-검색
+				ProcList.search();
+				break;
+			case "x": // 게시판 종료
+				Cw.wn("사이트 메인으로 이동");
+				break loop;
 			}
 		}
 	}
