b183a47d35ffacd5da47e34b6319bd52f654bdaf
JWH <44960019+hanjay3757@users.noreply.github.com>
2024-09-12 14:21:06 +0900

    Add files via upload

--- JSPdata/jspConnect.sql
+++ JSPdata/jspConnect.sql
@@ -1,14 +1,15 @@
-use my_cat;
+create database my_cats;
+use my_cats;
 
-select * from cat_board;
+
 create table cat_board(
-	num int primary key auto_increment, 
-    title char(255),
-    content text,
-    id char(30)
+	num int primary key auto_increment,title char(255),content text,id char(30)
 );
 
 insert into cat_board values(0,'테스트제목','테스트내용 내용.......','cat4');
 insert into cat_board values(0,'테스트제목2','테스트내용2 내용.......2','dog3');
 
+select * from cat_board;
+
 drop table cat_board;
+
