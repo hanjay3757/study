cbf991e8d635f9c4892799a6d6e469de64b9dcb6
JWH <44960019+hanjay3757@users.noreply.github.com>
2024-09-27 16:25:18 +0900

    servlet

---
+++ mySQL/servlet/src/main/java/test/test.java
@@ -0,0 +1,56 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class test
+ */
+@WebServlet("/test")
+public class test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("==== 두 겟");
+		System.out.println("id: " + req.getParameter("id"));
+		System.out.println("pw: " + req.getParameter("pw"));
+	}
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public test() {
+		super();
+		// TODO Auto-generated constructor stub
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//			throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		
+//	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+
+	}
+
+}
