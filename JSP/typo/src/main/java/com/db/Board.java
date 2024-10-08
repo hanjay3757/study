83b95e14afd69737855ee6fb65c32f938340a9f2
JWH <44960019+hanjay3757@users.noreply.github.com>
2024-10-08 16:53:29 +0900

    Delete JSP/typo directory

--- JSP/typo/src/main/java/com/db/Board.java
+++
@@ -1,7 +0,0 @@
-package com.db;
-
-public class Board {
-	static public final int LIST_AMOUNT = 5; // 하나의 리스트에 보일 글 수
-	static public final int PAGE_LINK_AMOUNT = 3; // 페이지 링크 한 블럭에 보일 페이지 링크 갯수
-
-}
