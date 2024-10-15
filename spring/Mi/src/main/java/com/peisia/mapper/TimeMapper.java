6cc4e78109116efdda8e8703ea6cd6bf437fcf60
JWH <44960019+hanjay3757@users.noreply.github.com>
2024-10-15 12:17:51 +0900

    setting

--- spring/Mi/src/main/java/com/peisia/mapper/TimeMapper.java
+++ spring/Mi/src/main/java/com/peisia/mapper/TimeMapper.java
@@ -3,7 +3,9 @@ package com.peisia.mapper;
 import org.apache.ibatis.annotations.Select;
 
 public interface TimeMapper {
-	// @Select("SELECT sysdate From dual")
+//	@Select("SELECT sysdate From dual")
 	@Select("SELECT now() From dual")
 	public String getTime();
+
+	public String getTime2();
 }
