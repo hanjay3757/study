07297f80d7cd62b84d73caa1d402a5f3a5c50822
JWH <44960019+hanjay3757@users.noreply.github.com>
2024-10-16 12:18:13 +0900

    first_Spring

---
+++ mySQL/database/spring_basic.sql
@@ -0,0 +1,12 @@
+use my_cat;
+
+create table tbl_test (		
+	no int,	
+	str_data varchar(50)	
+);		
+insert into tbl_test values(1,'1');		
+insert into tbl_test values(2,'2');		
+insert into tbl_test values(3,'100');		
+insert into tbl_test values(4,'HELLO WORLD');		
+		
+select * from tbl_test;		
