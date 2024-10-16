0a81cc6735f4374c2cfa60fac9d8df896b6bb579
JWH <44960019+hanjay3757@users.noreply.github.com>
2024-10-16 17:49:13 +0900

    반복적으로 타이핑 같은거라 1개만 올림

---
+++ spring/practice/Mi/src/main/java/com/peisia/mapper/TestMapper.java
@@ -0,0 +1,15 @@
+package com.peisia.mapper;
+
+import com.peisia.spring.dto.TestDto;
+
+public interface TestMapper {
+	public TestDto getData1();
+
+	public TestDto getData2();
+
+	public TestDto getData3();
+
+	public TestDto getData4();
+
+	public void updateVisitantCount();
+}
---
+++ spring/practice/Mi/src/main/java/com/peisia/service/TestServiceImpl.java
@@ -0,0 +1,42 @@
+package com.peisia.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.peisia.mapper.TestMapper;
+import com.peisia.spring.dto.TestDto;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+//@AllArgsConstructor
+public class TestServiceImpl implements TestService {
+
+	@Setter(onMethod_ = @Autowired)
+	private TestMapper mapper;
+
+	@Override
+	public String getOne() {
+		log.info("test===========");
+		TestDto tvo = mapper.getData1();
+		String one = tvo.getStr_data();
+		return one;
+	}
+
+	@Override
+	public String getTwo() {
+		log.info("test===========");
+		TestDto tvo = mapper.getData2();
+		String two = tvo.getStr_data();
+		return two;
+	}
+
+	/* 문제 1 */
+	@Override
+	public void updateVisitantCount() {
+		mapper.updateVisitantCount();
+	}
+
+}
