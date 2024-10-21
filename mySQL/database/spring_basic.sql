97bebe9046efdff627a15da6cb871371ef20fbfa
JWH <44960019+hanjay3757@users.noreply.github.com>
2024-10-21 17:24:14 +0900

    Add files via upload

--- mySQL/database/spring_basic.sql
+++ mySQL/database/spring_basic.sql
@@ -8,5 +8,22 @@ insert into tbl_test values(1,'1');
 insert into tbl_test values(2,'2');		
 insert into tbl_test values(3,'100');		
 insert into tbl_test values(4,'HELLO WORLD');		
+		create table tbl_guest(				
+	bno int auto_increment primary key,			
+	btext text			
+);				
 		
-select * from tbl_test;		
+create table tbl_visitant_count (			
+	count int		
+);		
+insert into tbl_guest (btext) values('개');				
+insert into tbl_guest (btext) values('고양이');					
+insert into tbl_visitant_count values(0);			
+select * from tbl_visitant_count;			
+select * from tbl_guest;		
+SHOW VARIABLES LIKE 'general_log%';
+show processlist; 
+ SET GLOBAL general_log = 'ON';
+ show variables where Variable_name in ('version', 'log', 'general_log');
+ cd /usr/local/mysql/data
+SET GLOBAL general_log_file='/var/lib/mysql/workbench/sql_actions_Local_instance_MySQL80.log.log';
