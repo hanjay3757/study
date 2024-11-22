3bc160a32bbf69857031a47ef3a57c7029cf8dfd
hanjay3757 <hanjay3757@gmail.com>
2024-11-22 17:46:49 +0900

    수정

--- reacts/src/reportWebVitals.js
+++ reacts/src/reportWebVitals.js
@@ -9,5 +9,6 @@ const reportWebVitals = onPerfEntry => {
     });
   }
 };
-
+//Web Vitals는 Google에서 제공하는 표준화된 지표들로, 웹사이트의 사용자 경험을 측정하는 데 중요한 역할을 합니다. 여기서 측정하는 주요 지표는 다음과 같습니다
+ 
 export default reportWebVitals;
